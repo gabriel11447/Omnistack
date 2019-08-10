f52609489d68c39a9737f9c4306bd27dda46d317
Gabriel <gabriel.de.carvalho_@hotmail.com>
2019-08-10 13:55:26 -0300

    Added mongoose

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,7 @@ const express = require('express');
 const routes = express.Router();
 
 routes.get('/', (req, res) => {
-    return res.json({ message: `Hello ${req.query.name}` });
+    return res.json({ message: `Olá, ${req.query.name}` });
 });
 
 routes.post('/devs', (req, res) => {
--- src/server.js
+++ src/server.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const routes = require('./routes');
 
 const server = express();
+
+mongoose.connect('mongodb+srv://gabriel11447:12ab12@cluster0-ses9o.mongodb.net/omnistack?retryWrites=true&w=majority', { 
+    useNewUrlParser: true
+});
 
 server.use(express.json());
 server.use(routes);
